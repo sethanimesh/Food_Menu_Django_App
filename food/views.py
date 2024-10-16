85bed5097c38965dffcda6d9654d7e3a1b3835e9
animesh <sethanimesh@hotmail.com>
2024-10-16 09:57:21 +0530

    display data

--- food/views.py
+++ food/views.py
@@ -1,5 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from food.models import Item
+
 def index(request):
-    return HttpResponse("Hello, world. You're at the food index.")
+    item_list = Item.objects.all()
+    return HttpResponse(item_list)
