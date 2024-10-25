38df085c220abf9e3a7d81ec9075745e9a467673
animesh <sethanimesh@hotmail.com>
2024-10-25 22:52:54 +0530

    detailed view of each item

--- food/views.py
+++ food/views.py
@@ -7,3 +7,6 @@ def index(request):
     item_list = Item.objects.all()
     context = {'item_list':item_list,}
     return render(request, 'food/index.html', context)
+
+def detail(request, item_id):
+    return HttpResponse("Item is %d"%item_id)
