ad0850f10f6cc5eb32935c63677ccf86405a94db
animesh <sethanimesh@hotmail.com>
2024-10-27 12:28:50 +0530

    adding image to model

--- food/models.py
+++ food/models.py
@@ -8,3 +8,4 @@ class Item(models.Model):
     item_name = models.CharField(max_length=200)
     item_desc = models.CharField(max_length=200)
     item_price = models.IntegerField()
+    item_image = models.CharField(max_length=500, default="https://cdn-icons-png.flaticon.com/512/1377/1377194.png")
